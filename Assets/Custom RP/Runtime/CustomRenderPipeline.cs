bb6a74e945755fa9488ab9da00c75720c5d6945e
zhangao <ouchzsc@gmail.com>
2023-03-17 01:45:17 +0800

    batch

--- Assets/Custom RP/Runtime/CustomRenderPipeline.cs
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -5,6 +5,11 @@ public class CustomRenderPipeline : RenderPipeline
 {
     private CameraRenderer _cameraRenderer = new CameraRenderer();
 
+    public CustomRenderPipeline()
+    {
+        GraphicsSettings.useScriptableRenderPipelineBatching = true;
+    }
+    
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
         foreach (var camera in cameras)
