7925abe7a92d9d2aa4a6582b3e002a3548c14647
zhangao <ouchzsc@gmail.com>
2023-03-17 01:39:16 +0800

    使用core package

--- Assets/Custom RP/Shader/UnlitPass.hlsl
+++ Assets/Custom RP/Shader/UnlitPass.hlsl
@@ -1,12 +1,15 @@
 #ifndef CUSTOM_UNLIT_PASS_INCLUDED
 #define CUSTOM_UNLIT_PASS_INCLUDED
+#include "../ShaderLibrary/Common.hlsl"
+
+CBUFFER_START(UnityPerMaterial)
+    float4 _BaseColor;
+CBUFFER_END
 
 float4 UnlitPassFragment():SV_TARGET
 {
-    return 0.0;
+    return _BaseColor;
 }
-
-#include "../ShaderLibrary/Common.hlsl"
 
 float4 UnlitPassVertex(float3 positionOS:POSITION):SV_POSITION
 {
