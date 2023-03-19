712ee4ce6eb1280a4c2a66b23ed7ced375d39b0b
zhangao <ouchzsc@gmail.com>
2023-03-20 01:32:29 +0800

    2.2 many color

---
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour {
+	
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    static MaterialPropertyBlock block;
+    
+    [SerializeField]
+    Color baseColor = Color.white;
+ 
+    void Awake () {
+	    OnValidate();
+    }
+     
+    private void OnValidate()
+    {
+
+	    if (block == null)
+		    block = new MaterialPropertyBlock();
+	    block.SetColor(baseColorId,baseColor);
+	    GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+}
